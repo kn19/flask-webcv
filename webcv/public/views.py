f3edcc394c21991aade94c6c57208944ed032247
Khoa Nguyen <kngyn19@gmail.com>
2017-09-24 07:24:16 -0500

    updated foldername

--- webcv/public/views.py
+++ webcv/public/views.py
@@ -2,7 +2,7 @@
 import json
 from flask import Blueprint, redirect, render_template, request, jsonify
 
-public = Blueprint('public', __name__, template_folder='../templates', static_folder='../static')
+public = Blueprint('public', __name__, template_folder='app', static_folder='app')
 
 @public.route('/')
 def main():
